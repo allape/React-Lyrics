be63f1f186d3f59e10596bab4126a2b1dd4b3197
as <allensnape@gmail.com>
2025-03-14 16:12:15 +0800

    chore: add more exports

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
+export { default as LyricsDriver } from "./src/core/lyrics";
+
 export { default as Lyrics, type ILyricsProps } from "./src/component/Lyrics";
+
 export {
   default as LyricsEditor,
   DefaultWordSplitterRegexp,
   type ILyricsEditorProps,
 } from "./src/component/LyricsEditor";
+
+export { default as useRAFAudioTime } from "./src/hook/useRAFAudioTime";
